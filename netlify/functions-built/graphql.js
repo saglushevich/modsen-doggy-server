83555b85c67309b523e85fcbae5150a169a9a881
Sergey Aglushevich <s.aglushevich5@gmail.com>
2025-07-29 01:33:44 +0300

    fix build

---
+++ netlify/functions-built/graphql.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+const apollo_1 = require("../../src/apollo");
+Object.defineProperty(exports, "handler", { enumerable: true, get: function () { return apollo_1.handler; } });
+//# sourceMappingURL=graphql.js.map
